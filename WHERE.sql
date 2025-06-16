14683d77da1db6154357250067ee9091857925e3
MD-TANZIM-EHSAN <mdtanzime@gmail.com>
2025-06-16 15:47:35 +0600

    Delete WHERE.sql

--- WHERE.sql
+++
@@ -1,8 +0,0 @@
--- Retrieve all customers who have a score greater than 500
-Select * 
-from customers
-where Score>=500 ;
--- Retrive customer from germany
-select * 
-from customers
-where Country ="Germany"
