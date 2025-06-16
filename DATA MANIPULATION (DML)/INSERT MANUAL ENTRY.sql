ee9aceaa4c95d432358bc2973e8ecb1ae7986578
MD-TANZIM-EHSAN <mdtanzime@gmail.com>
2025-06-16 19:48:39 +0600

    Add files via upload

---
+++ DATA MANIPULATION (DML)/INSERT MANUAL ENTRY.sql
@@ -0,0 +1,7 @@
+-- Manual Entry ---
+Insert into customers (CustomerID,FirstName,LastName,Country,Score) -- columns----------
+ -- $$$ columns and values must be in same order $$$ --------------
+ values 
+	   (6,'Tanzim','sam,','Bangladesh',NUll), 
+       (7,'Ehsan','tan','Bangladesh',600),
+       (8,'Zihad','jack','Bangladesh',NUll)
