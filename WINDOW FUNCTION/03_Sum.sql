a00632b8e6f886a5905077cd7b46f989ca1a6e75
MD-TANZIM-EHSAN <mdtanzime@gmail.com>
2025-06-21 12:58:20 +0600

    Update 03_Sum.sql

--- WINDOW FUNCTION/03_Sum.sql
+++ WINDOW FUNCTION/03_Sum.sql
@@ -9,4 +9,16 @@ select OrderID, CustomerID,Sales,ProductID,
 sum(Sales) over(),
 ROUND(Sales/sum(Sales) over() * 100,2 )
 from orders;
-select* from orders
+select* from orders;
+-- -------------------------------------- ANALYTICAL USECASES-----------------------------------------------------
+-- FIND OVERALL TOTAL
+-- Syntex:- Sum(SALES) OVER() -overview entire data 
+
+-- TOTAL PER Group
+-- Syntex:- Sum(Sales) OVER(PARTITION BY product) -- COMPARE  CATEGORY
+
+-- Running Total 
+-- Syntex :- SUM(Sales) over(order by MONTH) -- Progress over time
+
+-- Rolling total
+-- Syntex : - SUM(Sales) Over(order by MONTH ROWs BETWEEN 2 preceding AND CURRENT ROW ) -- progrs over time in specific fixed window
